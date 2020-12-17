435165da82b8687c827afa595ba0dfd4ea2b1445
Rachel Ho <rachelhoyuenyi@gmail.com>
2020-12-17 16:38:27 +0800

    nav & toast

--- src/screens/dashboard/components/room_nav/make_room/index.tsx
+++ src/screens/dashboard/components/room_nav/make_room/index.tsx
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from "react";
 import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
 import Button from "@material-ui/core/Button";
 import axios from "axios";
 
 export const MakeRoomForm = () => {
-  return <h1>hi</h1>;
+  const [inputs, setInputs] = useState({ username: "", roomcode: "" });
+  return (
+    <>
+      <div className="form">
+        <form noValidate autoComplete="off">
+          <FormHelperText id="component-helper-text">
+            Make a room
+          </FormHelperText>
+          <TextField
+            id="outlined-name"
+            label="Username"
+            name="username"
+            placeholder="Enter username"
+            value={inputs.username}
+            onChange={handleChange}
+            variant="outlined"
+          />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default MakeRoomForm;

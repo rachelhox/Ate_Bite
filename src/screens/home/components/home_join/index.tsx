ec6cb53dde19c71093b035e241b8507ad6888762
Mika Fung <fungcheukman@hotmail.com>
2020-12-19 14:42:26 +0800

    1

--- src/screens/home/components/home_join/index.tsx
+++ src/screens/home/components/home_join/index.tsx
@@ -6,8 +6,10 @@ import TextField from "@material-ui/core/TextField";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import Button from "@material-ui/core/Button";
 import logo from "../../../../static/icons/ate-bite-logo.png";
+import { useHistory } from "react-router-dom";
 
 export const HomeJoin = () => {
+  const history = useHistory();
   const [inputs, setInputs] = useState({ username: "", roomcode: "" });
   const handleChange = (event: any) => {
     const { name, value } = event.target;
@@ -35,6 +37,7 @@ export const HomeJoin = () => {
         .then((res) => {
           if (res.status === 200) {
             toast.success("success");
+            // history.push(`/room/${x.roomcode}/${user.userId}`);
           }
         })
         .catch((err) => {

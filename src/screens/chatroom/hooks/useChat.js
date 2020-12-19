f7371722e086848fcd6687356f7c96af65fc6b50
Arran Harding <arran101@gmail.com>
2020-12-19 14:41:11 +0800

    small changes

--- src/screens/chatroom/hooks/useChat.js
+++ src/screens/chatroom/hooks/useChat.js
@@ -6,8 +6,9 @@ const NEW_CHAT_MESSAGE_EVENT= 'newChatMessage'; //name of the event
 const NEW_FEED_MESSAGE_EVENT= 'newFeedMessage';
 const SERVER_URL='http://localhost:4000'
 
-// const user_Id = window.localStorage.getItem(userId)
-// console.log(user_Id)
+const users_id = window.localStorage.getItem('userId')
+console.log('original users ID:' + users_id)
+console.log(typeof(users_id))
 
 const UseChat = (roomId) => {
     
@@ -26,10 +27,13 @@ const UseChat = (roomId) => {
 
         Axios.post(`${SERVER_URL}/chatroom/existing`, { roomId })
         .then(function (response){
-            console.log(response.data[0])
+            // console.log(response.data[0])
             for (let i = 0; i < response.data.length; i++) {
-                console.log(response.data[i].message)
-                let incomingInfo = response.data[i]
+                
+                let incomingInfo ={...response.data[i],
+                    ownedByCurrentUser:response.data[i].users_id == users_id,
+                }
+                console.log(incomingInfo)
                 setMessages((messages)=> [...messages, incomingInfo]);
               }
         })
@@ -39,7 +43,7 @@ const UseChat = (roomId) => {
             //  console.log(message)
             const incomingMessage = {
                 ...message,
-                ownedByCurrentUser: message.senderId === socketRef.current.id,
+                ownedByCurrentUser: message.users_id === users_id,
             }
             setMessages((messages)=> [...messages, incomingMessage]);
             // console.log(incomingMessage)
@@ -59,12 +63,12 @@ const UseChat = (roomId) => {
         socketRef.current.emit(NEW_CHAT_MESSAGE_EVENT,{
             message: messageBody,
             rooms_id: roomId,
-            senderId: socketRef.current.id,
+            users_id: users_id,
         });
         //this is actually an issue, because if the message fails to upload to the db and so gives an error, this will still emit to the feed. this will do for now, but later need to have this emitting once the message is received, not on sent- can put received into variable like this sendMessage, put the below emit into it and return the variable?
         socketRef.current.emit(NEW_FEED_MESSAGE_EVENT,{
-            body: `${socketRef.current.id} has sent a message`,
-            senderId: socketRef.current.id,
+            body: `${users_id} has sent a message`,
+            senderId: users_id,
         });
     };
     

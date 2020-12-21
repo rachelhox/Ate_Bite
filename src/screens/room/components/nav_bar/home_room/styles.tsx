158bddf5724313bbf51cdd9fba7bf24ab9b1e16d
Rachel Ho <rachelhoyuenyi@gmail.com>
2020-12-21 19:13:21 +0800

    clean up

--- src/screens/room/components/nav_bar/home_room/styles.tsx
+++ src/screens/room/components/nav_bar/home_room/styles.tsx
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const RoomHomeCSS = styled.div`
   height: 80vh;
-
   overflow: scroll;
+  //   width: fit
 `;
 
 export const RoomListCSS = styled.div`

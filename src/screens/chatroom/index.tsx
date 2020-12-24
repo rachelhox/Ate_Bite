1b408440520d8046f2855123e7b0a995c7bc76b8
Arran Harding <arran101@gmail.com>
2020-12-24 10:57:03 +0800

    saving

--- src/screens/chatroom/index.tsx
+++ src/screens/chatroom/index.tsx
@@ -2,112 +2,107 @@ import React from "react";
 // import Head from "next/head";
 // import { NavBar } from "@components";
 import { ChatroomCSS } from "./styles";
-import useChat from './hooks/useChat';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import useChat from "./hooks/useChat";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
 const Chat = () => {
-  
-    // const gettingParams = window.location.href.replaceAll("/", " ").split(" ");
-    // const roomcode = gettingParams[gettingParams.length-2];
-    // // get user id
-    // const users_id = gettingParams[gettingParams.length-1];
+  // const gettingParams = window.location.href.replaceAll("/", " ").split(" ");
+  // const roomcode = gettingParams[gettingParams.length-2];
+  // // get user id
+  // const users_id = gettingParams[gettingParams.length-1];
 
-    const roomcode = localStorage.getItem("roomcode");
-    const users_id = localStorage.getItem("userId");
+  const roomcode = localStorage.getItem("roomcode");
+  const users_id = localStorage.getItem("userId");
 
-    const { messages, sendMessage, messagesEndRef } = useChat(roomcode); //creates a websocket and manages messaging
-    const [newMessage, setNewMessage] = React.useState(''); //setting the messages to be sent 
-  
-    const handleNewMessageChange = (event) => {
-      setNewMessage(event.target.value);
-    };
+  const { messages, sendMessage, messagesEndRef } = useChat(roomcode); //creates a websocket and manages messaging
+  const [newMessage, setNewMessage] = React.useState(""); //setting the messages to be sent
 
-    const handleSendMessage = () => {
-      sendMessage(newMessage);
-      setNewMessage('');
-    };
-  
-    // const onKeyPress= (e) => {
-    //   if (e.key === 'Enter') {
-    //     sendMessage(newMessage);
-    //     setNewMessage('');
-    //   }
-    // }
+  const handleNewMessageChange = (event) => {
+    setNewMessage(event.target.value);
+  };
 
-    return(
-      <ChatroomCSS>
-        <div className="chatroom">
-          <div className='chatContainer'>
-            {/* <h1 className='roomName'>Room: {roomcode}</h1> */}
-            <div className='messagesContainer'>
-              <ol className='messagesList'>
-                <div className='messagesPosition'>
-                {messages.map((message, i)=>{
-                  return (
-                  (message.users_id == users_id ? 
-                     (<div>
-                      <li
-                        key={i}
-                        className={`messageItem myMessage`}>
-                          <h2 className="chat-text">{message.username}</h2>
-                          <hr className="chat-hr" />
-                          <h3 className="chat-text">{message.message}</h3>
-                          <h5 className="chat-text">{message.time}</h5>                         
-                      </li>
-                    </div>) :
-                    (<div>
-                    <li
-                      key={i}
-                      className={`messageItem receivedMessage`}>
+  const handleSendMessage = () => {
+    sendMessage(newMessage);
+    setNewMessage("");
+  };
+
+  // const onKeyPress= (e) => {
+  //   if (e.key === 'Enter') {
+  //     sendMessage(newMessage);
+  //     setNewMessage('');
+  //   }
+  // }
+
+  return (
+    <ChatroomCSS>
+      <div className="chatroom">
+        <div className="chatContainer">
+          {/* <h1 className='roomName'>Room: {roomcode}</h1> */}
+          <div className="messagesContainer">
+            <ol className="messagesList">
+              <div className="messagesPosition">
+                {messages.map((message, i) => {
+                  return message.users_id == users_id ? (
+                    <div>
+                      <li key={i} className={`messageItem myMessage`}>
                         <h2 className="chat-text">{message.username}</h2>
                         <hr className="chat-hr" />
                         <h3 className="chat-text">{message.message}</h3>
-                        <h5 className="chat-text">{message.time}</h5>                       
-                    </li>
-                  </div>)            
-                  ))
+                        <h5 className="chat-text">{message.time}</h5>
+                      </li>
+                    </div>
+                  ) : (
+                    <div>
+                      <li key={i} className={`messageItem receivedMessage`}>
+                        <h2 className="chat-text">{message.username}</h2>
+                        <hr className="chat-hr" />
+                        <h3 className="chat-text">{message.message}</h3>
+                        <h5 className="chat-text">{message.time}</h5>
+                      </li>
+                    </div>
+                  );
                 })}
-                </div>
-                <div ref={messagesEndRef} />
-              </ol>
-            </div>
+              </div>
+              <div ref={messagesEndRef} />
+            </ol>
           </div>
-            {/* <textarea
+        </div>
+        {/* <textarea
               value={newMessage}
               onChange={handleNewMessageChange}
               placeholder='Write message...'
               className='newMessageInputField'
             /> */}
-          <div className="chat-bottom">
-            <TextField
-              id="filled-multiline-static"
-              // label="Multiline"
-              multiline
-              rows={4}
-              value={newMessage}
-              placeholder='Write message...'
-              variant="filled"
-              onChange={handleNewMessageChange}
-            />
-              {/* <button onClick={handleSendMessage} className='sendMessageButton'>
+        <div className="chat-bottom">
+          <TextField
+            id="filled-multiline-static"
+            // label="Multiline"
+            multiline
+            rows={4}
+            value={newMessage}
+            placeholder="Write message..."
+            variant="filled"
+            onChange={handleNewMessageChange}
+          />
+          {/* <button onClick={handleSendMessage} className='sendMessageButton'>
                 Send
               </button> */}
-            <Button
-                variant="contained"
-                disabled ={!newMessage}
-                color="primary"
-                // classes={{ root: classesBtn.root }}
-                onClick={handleSendMessage}
-                // onKeyPress={onKeyPress}
-              >
-              send
-            </Button>
-          </div>
+          <Button
+            variant="contained"
+            disabled={!newMessage}
+            color="primary"
+            // classes={{ root: classesBtn.root }}
+            onClick={handleSendMessage}
+            // onKeyPress={onKeyPress}
+          >
+            send
+          </Button>
+        </div>
       </div>
-     </ChatroomCSS>
-    );
-  };
-  
-  export default Chat;
+    </ChatroomCSS>
+  );
+};
+
+export default Chat;
--- src/screens/voting/hooks/useCount.js
+++ src/screens/voting/hooks/useCount.js
@@ -1,88 +1,88 @@
-import Axios from 'axios';
-import { useEffect, useRef, useState } from 'react';
-import socketIOClient from 'socket.io-client';
+// import Axios from 'axios';
+// import { useEffect, useRef, useState } from 'react';
+// import socketIOClient from 'socket.io-client';
 
-const NEW_COUNT_EVENT= 'newCountOption';
-const NEW_FEED_MESSAGE_EVENT= 'newFeedCountOption';
-const SERVER_URL='http://localhost:4000' 
+// const NEW_COUNT_EVENT= 'newCountOption';
+// const NEW_FEED_MESSAGE_EVENT= 'newFeedCountOption';
+// const SERVER_URL='http://localhost:4000' 
 
-// const users_id = window.localStorage.getItem('userId')
-const gettingParams = window.location.href.replaceAll("/", " ").split(" ");
-  const users_id = gettingParams[gettingParams.length-1]
+// // const users_id = window.localStorage.getItem('userId')
+// const gettingParams = window.location.href.replaceAll("/", " ").split(" ");
+//   const users_id = gettingParams[gettingParams.length-1]
 
-const UseCount = (roomcode) => {
+// const UseCount = (roomcode) => {
     
-    const [addCount, setAddCount] = useState([]);
+//     const [addCount, setAddCount] = useState([]);
 
-    const socketRef = useRef();
+//     const socketRef = useRef();
 
-    useEffect(()=>{
-        //creates a WebSocket connection
-      socketRef.current = socketIOClient(SERVER_URL, {
-            query: { roomcode }, 
-            transports: ['websocket'],
-        });
+//     useEffect(()=>{
+//         //creates a WebSocket connection
+//       socketRef.current = socketIOClient(SERVER_URL, {
+//             query: { roomcode }, 
+//             transports: ['websocket'],
+//         });
 
-        Axios.post(`${SERVER_URL}/voting/count/existing`, { roomcode })
-        .then(function (response){
-            // console.log(response.data)
-            for (let i = 0; i < response.data.length; i++) {
-                // console.log(response.data[i].resto)
-                let incomingInfo = response.data[i]
-                setAddCount((countOption)=> [...countOption, incomingInfo]);
-              }
-        })
+//         Axios.post(`${SERVER_URL}/voting/count/existing`, { roomcode })
+//         .then(function (response){
+//             // console.log(response.data)
+//             for (let i = 0; i < response.data.length; i++) {
+//                 // console.log(response.data[i].resto)
+//                 let incomingInfo = response.data[i]
+//                 setAddCount((countOption)=> [...countOption, incomingInfo]);
+//               }
+//         })
 
-        // listens for incoming votingOption
-        socketRef.current.on(NEW_COUNT_EVENT, (addCount)=>{ 
-        //    console.log(count.vote.userID)
-            let voteCount = addCount.vote.userID
-            // console.log(voteCount)
-            let userHasVoted = false 
+//         // listens for incoming votingOption
+//         socketRef.current.on(NEW_COUNT_EVENT, (addCount)=>{ 
+//         //    console.log(count.vote.userID)
+//             let voteCount = addCount.vote.userID
+//             // console.log(voteCount)
+//             let userHasVoted = false 
            
-           for(let i = 0; i < voteCount.length; i++){
-            if(users_id == voteCount[i]){
-                console.log( voteCount[i])
-                userHasVoted = true
-                }
-            }
-            // console.log(userHasVoted)
-            const incomingCount = {
-                ...addCount, 
-                userHasVoted
-            }
-            console.log(incomingCount)
-            setAddCount((countOption)=>{
-                console.log(countOption)
-                for(let f = 0; f < countOption.length; f++){
-                    if(countOption[f].id == incomingCount.id){
-                        countOption[f] = incomingCount
-                    }
-                }
-            });
-            // socketRef.current.emit(NEW_FEED_MESSAGE_EVENT,{
-            //     body: `${socketRef.current.id} has made a new option to vote on`,
-            //     senderId: socketRef.current.id,
-            // });
-        });
+//            for(let i = 0; i < voteCount.length; i++){
+//             if(users_id == voteCount[i]){
+//                 console.log( voteCount[i])
+//                 userHasVoted = true
+//                 }
+//             }
+//             // console.log(userHasVoted)
+//             const incomingCount = {
+//                 ...addCount, 
+//                 userHasVoted
+//             }
+//             console.log(incomingCount)
+//             setAddCount((countOption)=>{
+//                 console.log(countOption)
+//                 for(let f = 0; f < countOption.length; f++){
+//                     if(countOption[f].id == incomingCount.id){
+//                         countOption[f] = incomingCount
+//                     }
+//                 }
+//             });
+//             // socketRef.current.emit(NEW_FEED_MESSAGE_EVENT,{
+//             //     body: `${socketRef.current.id} has made a new option to vote on`,
+//             //     senderId: socketRef.current.id,
+//             // });
+//         });
 
-        //destroys socket reference when connection is closed
-        return ()=> {
-            socketRef.current.disconnect();
-        };
-    }, []);
+//         //destroys socket reference when connection is closed
+//         return ()=> {
+//             socketRef.current.disconnect();
+//         };
+//     }, []);
 
-    const sendCount = (data) => {
-        // console.log(data)
-        socketRef.current.emit(NEW_COUNT_EVENT,{
-            data,
-            users_id,
-        });
+//     const sendCount = (data) => {
+//         // console.log(data)
+//         socketRef.current.emit(NEW_COUNT_EVENT,{
+//             data,
+//             users_id,
+//         });
      
-    };
+//     };
     
     
-    return { addCount, sendCount };
-};
+//     return { addCount, sendCount };
+// };
 
-export default UseCount;
+// export default UseCount;
